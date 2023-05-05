48783669e6705f4001e9905c7395c83b408557c7
SemihKokcu <semihkkc1@gmail.com>
2023-05-05 11:20:22 +0300

    basics-redirect

--- basics/app.js
+++ basics/app.js
@@ -3,10 +3,16 @@ const http = require('http')
 
 
 const server = http.createServer((req,res)=>{
-    // console.log(req)
-    console.log(req.url,req.method,req.headers)
-    // process.exit()
-    
+
+    const url = req.url;
+    if (url==='/') {
+        res.setHeader('Content-Type','text/html')
+        res.write('<html>')
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><form method="POST" action="/message"> <input type="text" name="message"/> <button type="submit">Gonder</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
     res.setHeader('Content-Type','text/html')
     res.write('<html>')
     res.write('<head><title>My First Page</title></head>')
@@ -14,6 +20,10 @@ const server = http.createServer((req,res)=>{
     res.write('</html>')
     res.end()
     
+    
 });
 
 server.listen(3000);
+
+   
+   
