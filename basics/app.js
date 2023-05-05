dca68ccb2f890d3dd47d63e46218d1b9082aedf8
SemihKokcu <semihkkc1@gmail.com>
2023-05-05 11:26:19 +0300

    basics-redirecting

--- basics/app.js
+++ basics/app.js
@@ -1,10 +1,12 @@
 const http = require('http')
-
+const fs = require('fs');
 
 
 const server = http.createServer((req,res)=>{
 
     const url = req.url;
+    const method =req.method;
+
     if (url==='/') {
         res.setHeader('Content-Type','text/html')
         res.write('<html>')
@@ -13,13 +15,18 @@ const server = http.createServer((req,res)=>{
         res.write('</html>')
         return res.end()
     }
-    res.setHeader('Content-Type','text/html')
-    res.write('<html>')
-    res.write('<head><title>My First Page</title></head>')
-    res.write('<body><h1>Hello from Semih this is my Node.js Server!</h1></body>')
-    res.write('</html>')
-    res.end()
-    
+    if (url==="/message" && method ==='POST') {
+        fs.writeFileSync('message.txt','DUMMY');
+        res.statusCode=302;
+        res.writeHead('Location','/');
+        return res.end(); 
+    }
+        res.setHeader('Content-Type','text/html')
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><h1>Hello from Semih this is my Node.js Server!</h1></body>')
+        res.write('</html>')
+        res.end()
     
 });
 
