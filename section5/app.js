d4f3631e6bc860486c4c47b058c064b617337d38
SemihKokcu <semihkkc1@gmail.com>
2023-05-08 09:56:36 +0300

    parse routes

--- section5/app.js
+++ section5/app.js
@@ -2,22 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express();
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false})); //body parser middleware
 
-app.use("/add-product",(req,res,next)=>{
-     console.log("in the middleware!")
-     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-})
-
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
-app.use("/",(req,res,next)=>{
-    res.send('<h1>Hello</h1>')
-})
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000)
