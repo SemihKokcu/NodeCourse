a3ac506433b5f23ece2bf30e23460b910afcdce3
SemihKokcu <semihkkc1@gmail.com>
2023-05-08 10:22:54 +0300

    create views

--- section5/app.js
+++ section5/app.js
@@ -7,7 +7,7 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false})); //body parser middleware
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 
