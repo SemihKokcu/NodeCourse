f61f2047bffe012609e4d41c669d640a5a5d70af
SemihKokcu <semihkkc1@gmail.com>
2023-05-09 10:41:33 +0300

    fetch product and list

--- shop-project/util/database.js
+++ shop-project/util/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-course',
-    password:'315312'
-})
+    host: 'localhost',
+    user: 'root',
+    database: 'node-course',
+    password: '315312'
+});
 
-module.exports = pool.promise()
+module.exports = pool.promise();
