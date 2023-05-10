238ba3dcf8657b5691585231def82c2f207004e5
SemihKokcu <semihkkc1@gmail.com>
2023-05-10 16:17:00 +0300

    logout

--- shop-project/app.js
+++ shop-project/app.js
@@ -37,7 +37,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  User.findById('645b7151b015477706002db2')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
