e9898b6986e16d23ed26946469966014965e0a6e
SemihKokcu <semihkkc1@gmail.com>
2023-05-10 10:48:13 +0300

    add cart and delete cart in user

--- shop-project/app.js
+++ shop-project/app.js
@@ -21,7 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('645b43b0348230d63dc5babd')
     .then(user => {
-      req.user = user;
+      if (!user.cart) {
+        user.cart = { items: [] };
+      }
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
